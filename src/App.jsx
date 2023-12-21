7e2a8f5fb75d779823af4ba4860007b489e9f0be
aconstas <82304192+aconstas@users.noreply.github.com>
2023-12-20 17:41:51 -0800

    add shuffleCards function on card click

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import "./App.css";
 import "./Card.css";
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
+import Scoreboard from "./components/Scoreboard";
 
 export default function App() {
   const [characters, setCharacters] = useState([]);
+  const [selectedCharacters, setSelectedCharacters] = useState([]);
+  const [bestScore, setBestScore] = useState(0);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -12,12 +15,52 @@ export default function App() {
       .then((data) => setCharacters(data.results));
   }, []);
 
+  console.log(selectedCharacters);
+
+  function handleCardClick(character) {
+    setSelectedCharacters([...selectedCharacters, character.name]);
+    if (isCardAlreadyClicked(character)) {
+      updateBestScore();
+      return alert("You lost!");
+    } else {
+      shuffleCards();
+    }
+  }
+
+  function isCardAlreadyClicked(character) {
+    return selectedCharacters.includes(character.name);
+  }
+
+  function shuffleCards() {
+    let shuffledCharacters = [...characters];
+
+    for (let i = shuffledCharacters.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [shuffledCharacters[i], shuffledCharacters[randomIndex]] = [
+        shuffledCharacters[randomIndex],
+        shuffledCharacters[i],
+      ];
+    }
+    setCharacters(shuffledCharacters);
+  }
+
+  function updateBestScore() {
+    if (selectedCharacters.length > bestScore) {
+      setBestScore(selectedCharacters.length);
+    }
+  }
+
   return (
     <>
       <h1>MEMORY GAME</h1>
+      <Scoreboard score={selectedCharacters.length} bestScore={bestScore} />
       <div className="cards-container">
         {characters.map((character) => (
-          <Card key={character.id} character={character} />
+          <Card
+            key={character.id}
+            character={character}
+            handleCardClick={() => handleCardClick(character)}
+          />
         ))}
       </div>
     </>
